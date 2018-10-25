358eb5d30b49dbe4b469d055a172a57be5d28ebe
ChatpholT <ChatpholT@ATA-LT084.ATA-BKK.local>
2018-10-25 16:22:26 +0700

    Update Menu Education & Skills

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -13,17 +13,13 @@ $(document).ready(function () {
     });
 
     $(".next-step").click(function (e) {
-
         var $active = $('.wizard .nav-tabs li.active');
         $active.next().removeClass('disabled');
         nextTab($active);
-
     });
     $(".prev-step").click(function (e) {
-
         var $active = $('.wizard .nav-tabs li.active');
         prevTab($active);
-
     });
     
     $("#card-id-eff-date, #card-id-end-date, #dob").datepicker({
@@ -39,28 +35,52 @@ $(document).ready(function () {
     $('#card-id-eff-date, #card-id-end-date, #dob').datepicker('update', '17/08/2561');
 
     //@naresh action dynamic childs
-    var next = 0;
-    $("#add-more").click(function(e){
+    var edu_form_next = 0;
+    $("#edu-form-add-more").click(function(e){
         e.preventDefault();
-        var addto = "#field" + next;
-        var addRemove = "#field" + (next);
-        next = next + 1;
-        var newIn = ' <div id="field'+ next +'" name="field'+ next +'"><!-- Text input--><div class="form-group"> <label class="col-md-4 control-label" for="action_id">Action Id</label> <div class="col-md-5"> <input id="action_id" name="action_id" type="text" placeholder="" class="form-control input-md"> </div></div><br><br> <!-- Text input--><div class="form-group"> <label class="col-md-4 control-label" for="action_name">Action Name</label> <div class="col-md-5"> <input id="action_name" name="action_name" type="text" placeholder="" class="form-control input-md"> </div></div><br><br><!-- File Button --> <div class="form-group"> <label class="col-md-4 control-label" for="action_json">Action JSON File</label> <div class="col-md-4"> <input id="action_json" name="action_json" class="input-file" type="file"> </div></div></div>';
+        var addto = "#field" + edu_form_next;
+        var addRemove = "#field" + (edu_form_next);
+        edu_form_next = edu_form_next + 1;
+        var newIn = '<div id="field'+ edu_form_next +'" class="p-t-25"><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="action_id">ชื่อสถานศึกษา :</label><div class="col-md-8"><input id="school-name" name="school-name" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">สาขาวิชา :</label><div class="col-md-8"><input id="discipline" name="discipline" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">เริ่มปี พ.ศ. :</label><div class="col-md-8"><input id="eff-academic-year" name="eff-academic-year" type="text" placeholder="25xx" class="form-control input-md w-75-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">สำเร็จปี พ.ศ. :</label><div class="col-md-8"><input id="" name="end-academic-year" type="end-academic-year" placeholder="25xx" class="form-control input-md w-75-px"></div></div><br><br></div>';
         var newInput = $(newIn);
-        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >Remove</button></div></div><div id="field">';
+        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="remove' + (edu_form_next - 1) + '" class="btn btn-danger edu-remove" >Remove</button></div></div>';
         var removeButton = $(removeBtn);
         $(addto).after(newInput);
         $(addRemove).after(removeButton);
-        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-        $("#count").val(next);  
-        
-            $('.remove-me').click(function(e){
-                e.preventDefault();
-                var fieldNum = this.id.charAt(this.id.length-1);
-                var fieldID = "#field" + fieldNum;
-                $(this).remove();
-                $(fieldID).remove();
-            });
+        $("#field" + edu_form_next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(edu_form_next);
+        $('.edu-remove').click(function(e){
+            e.preventDefault();
+            var fieldNum = this.id.charAt(this.id.length-1);
+            var fieldID = "#field" + fieldNum;
+            console.log(fieldID);
+            $(this).remove();
+            $(fieldID).remove();
+        });
+    });
+
+    var skill_form_next = 0;
+    $("#skill-form-add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#skill-field" + skill_form_next;
+        var addRemove = "#skill-field" + (skill_form_next);
+        skill_form_next = skill_form_next + 1;
+        var newIn = '<div id="skill-field'+ skill_form_next +'" class="p-t-25"><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="action_id">ภาษา :</label><div class="col-md-8"><input id="school-name" name="school-name" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">พูด :</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">อ่าน :</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">เขียน</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div></div>';
+        var newInput = $(newIn);
+        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="remove' + (skill_form_next - 1) + '" class="btn btn-danger skill-remove" >Remove</button></div></div>';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#skill-field" + skill_form_next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(skill_form_next);
+        $('.skill-remove').click(function(e){
+            e.preventDefault();
+            var fieldNum = this.id.charAt(this.id.length-1);
+            var fieldID = "#skill-field" + fieldNum;
+            console.log(fieldID);
+            $(this).remove();
+            $(fieldID).remove();
+        });
     });
     
 });
@@ -71,4 +91,3 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
-
