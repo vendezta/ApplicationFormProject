49a4a64181950bd9c51723966384ddc3a2e3f23b
ChatpholT <ChatpholT@ATA-LT084.ATA-BKK.local>
2018-10-26 14:57:25 +0700

    Update Education menu

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -38,21 +38,21 @@ $(document).ready(function () {
     var edu_form_next = 0;
     $("#edu-form-add-more").click(function(e){
         e.preventDefault();
-        var addto = "#field" + edu_form_next;
-        var addRemove = "#field" + (edu_form_next);
+        var addto = "#edu-field" + edu_form_next;
+        var addRemove = "#edu-field" + (edu_form_next);
         edu_form_next = edu_form_next + 1;
-        var newIn = '<div id="field'+ edu_form_next +'" class="p-t-25"><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="action_id">ชื่อสถานศึกษา :</label><div class="col-md-8"><input id="school-name" name="school-name" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">สาขาวิชา :</label><div class="col-md-8"><input id="discipline" name="discipline" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">เริ่มปี พ.ศ. :</label><div class="col-md-8"><input id="eff-academic-year" name="eff-academic-year" type="text" placeholder="25xx" class="form-control input-md w-75-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">สำเร็จปี พ.ศ. :</label><div class="col-md-8"><input id="" name="end-academic-year" type="end-academic-year" placeholder="25xx" class="form-control input-md w-75-px"></div></div><br><br></div>';
+        var newIn = '<div id="field'+ edu_form_next +'" class="p-t-25"><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="action_id">ชื่อสถานศึกษา :</label><div class="col-md-8"><input name="school-name" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">สาขาวิชา :</label><div class="col-md-8"><input id="discipline" name="discipline" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">เริ่มปี พ.ศ. :</label><div class="col-md-8"><input id="eff-academic-year" name="eff-academic-year" type="text" placeholder="25xx" class="form-control input-md w-75-px"></div></div><br><br><!-- Text input--><div><label class="col-md-2 col-md-offset-2 control-label" for="discipline">สำเร็จปี พ.ศ. :</label><div class="col-md-8"><input id="" name="end-academic-year" type="end-academic-year" placeholder="25xx" class="form-control input-md w-75-px"></div></div><br><br></div>';
         var newInput = $(newIn);
-        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="remove' + (edu_form_next - 1) + '" class="btn btn-danger edu-remove" >Remove</button></div></div>';
+        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="edu-remove' + (edu_form_next - 1) + '" class="btn btn-danger edu-remove" >Remove</button></div></div>';
         var removeButton = $(removeBtn);
         $(addto).after(newInput);
         $(addRemove).after(removeButton);
-        $("#field" + edu_form_next).attr('data-source',$(addto).attr('data-source'));
+        $("#edu-field" + edu_form_next).attr('data-source',$(addto).attr('data-source'));
         $("#count").val(edu_form_next);
         $('.edu-remove').click(function(e){
             e.preventDefault();
             var fieldNum = this.id.charAt(this.id.length-1);
-            var fieldID = "#field" + fieldNum;
+            var fieldID = "#edu-field" + fieldNum;
             console.log(fieldID);
             $(this).remove();
             $(fieldID).remove();
@@ -65,9 +65,9 @@ $(document).ready(function () {
         var addto = "#skill-field" + skill_form_next;
         var addRemove = "#skill-field" + (skill_form_next);
         skill_form_next = skill_form_next + 1;
-        var newIn = '<div id="skill-field'+ skill_form_next +'" class="p-t-25"><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="action_id">ภาษา :</label><div class="col-md-8"><input id="school-name" name="school-name" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">พูด :</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">อ่าน :</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">เขียน</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div></div>';
+        var newIn = '<div id="skill-field'+ skill_form_next +'" class="p-t-25"><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="action_id">ภาษา :</label><div class="col-md-8"><input name="skill-name" type="text" placeholder="" class="form-control input-md w-400-px"></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">พูด :</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langSpeakingLv" name="langSpeakingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">อ่าน :</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langReadingLv" name="langReadingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div><!-- Text input--><div class="row m-0"><label class="text-right col-md-2 col-md-offset-2 control-label" for="discipline">เขียน</label><div class="col-md-8"><div class="row"><div class="col-md-3"><input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">ดี&nbsp;&nbsp;<input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">พอใช้&nbsp;&nbsp;<input id="langWrittingLv" name="langWrittingLv" type="radio" placeholder="" class="input-md ">น้อย</div><div class="col-md-9"></div></div></div></div></div>';
         var newInput = $(newIn);
-        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="remove' + (skill_form_next - 1) + '" class="btn btn-danger skill-remove" >Remove</button></div></div>';
+        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="skill-remove' + (skill_form_next - 1) + '" class="btn btn-danger skill-remove" >Remove</button></div></div>';
         var removeButton = $(removeBtn);
         $(addto).after(newInput);
         $(addRemove).after(removeButton);
@@ -77,6 +77,31 @@ $(document).ready(function () {
             e.preventDefault();
             var fieldNum = this.id.charAt(this.id.length-1);
             var fieldID = "#skill-field" + fieldNum;
+            console.log(fieldID);
+            $(this).remove();
+            $(fieldID).remove();
+        });
+    });
+
+    var comp_skill_form_next = 0;
+    $("#comp-skill-form-add-more").click(function(e){
+        console.log("comp-skill-form-add-more");
+        e.preventDefault();
+        var addto = "#comp-skill-field" + comp_skill_form_next;
+        var addRemove = "#comp-skill-field" + (comp_skill_form_next);
+        comp_skill_form_next = comp_skill_form_next + 1;
+        var newIn = '<div id="comp-skill-field'+ comp_skill_form_next +'" class="row p-b-5 p-t-25 text-right"><!-- Text input--><div class="row m-0">	<label class="col-md-2 col-md-offset-2 control-label" for="action_id">ภาษา :</label><div class="col-md-8"><input name="comp-skill-name" type="text" placeholder="" class="form-control input-md w-400-px">	</div></div></div>';
+        var newInput = $(newIn);
+        var removeBtn = '<div class="row"><div class="col-md-9 col-offset-md-3 text-right"><button id="comp-skill-remove' + (comp_skill_form_next - 1) + '" class="btn btn-danger skill-remove" >Remove</button></div></div>';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#comp-skill-field" + comp_skill_form_next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(comp_skill_form_next);
+        $('.skill-remove').click(function(e){
+            e.preventDefault();
+            var fieldNum = this.id.charAt(this.id.length-1);
+            var fieldID = "#comp-skill-field" + fieldNum;
             console.log(fieldID);
             $(this).remove();
             $(fieldID).remove();
